9fcf2c6802eaab4b79d190d4c7f5e559229177a1
Erinary <udalova.lbv@gmail.com>
2017-07-12 20:10:51 +0700

    Реализована первая версия задачи.

--- Sort/src/ru/academits/erinary/main/Main.java
+++ Sort/src/ru/academits/erinary/main/Main.java
@@ -1,14 +1,129 @@
 package ru.academits.erinary.main;
 
 import ru.academits.erinary.sort.ContentType;
+import ru.academits.erinary.sort.SortException;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    private static void sortFile(String in, String out, ContentType contentType, boolean ascendingSort) {
+    private static final int MAX_LIST_CAPACITY = 100;
 
+    private static <T> void sortList(List<T> list, Comparator<T> cmp) {
+        for (int i = 1; i < list.size(); ++i) {
+            T temp = list.get(i);
+            for (int j = i - 1; j >= 0; --j) {
+                if (j == 0) {
+                    if (cmp.compare(list.get(j), temp) <= 0) {
+                        list.set(j + 1, temp);
+                    } else {
+                        list.set(j + 1, list.get(j));
+                        list.set(j, temp);
+                    }
+                } else if (cmp.compare(temp, list.get(j)) >= 0) {
+                    list.set(j + 1, temp);
+                    break;
+                } else {
+                    list.set(j + 1, list.get(j));
+                }
+            }
+        }
+    }
+
+    private static class AscIntComparator implements Comparator<Integer> {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            if (o1 > o2) {
+                return 1;
+            } else if (o1 < o2) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    private static class DescIntComparator implements Comparator<Integer> {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            if (o1 > o2) {
+                return -1;
+            } else if (o1 < o2) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    private static class AscStringComparator implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            return o1.compareTo(o2);
+        }
+    }
+
+    private static class DescStringComparator implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            return -o1.compareTo(o2);
+        }
+    }
+
+    private static void sortFile(String in, String out, ContentType contentType, boolean ascendingSort) throws SortException {
+        if (contentType == ContentType.INT) {
+            List<String> listString = Main.readStringInput(in);
+            List<Integer> outList = Main.parseIntList(listString);
+            Comparator<Integer> cmp = (ascendingSort) ? new AscIntComparator() : new DescIntComparator();
+            Main.sortList(outList, cmp);
+            Main.writeOutput(outList, out);
+        } else {
+            List<String> outList = Main.readStringInput(in);
+            Comparator<String> cmp = (ascendingSort) ? new AscStringComparator() : new DescStringComparator();
+            Main.sortList(outList, cmp);
+            Main.writeOutput(outList, out);
+        }
+    }
+
+    private static List<String> readStringInput(String inputFile) throws SortException {
+        try (Scanner scanner = new Scanner(new FileInputStream(inputFile))) {
+            List<String> listString = new ArrayList<>(MAX_LIST_CAPACITY);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                listString.add(line);
+            }
+            return listString;
+        } catch (FileNotFoundException e) {
+            throw new SortException("Файл не найден!");
+        }
+    }
+
+    private static List<Integer> parseIntList(List<String> listString) throws SortException {
+        List<Integer> listInt = new ArrayList<>(listString.size());
+        for (String s : listString) {
+            try {
+                Integer number = Integer.valueOf(s);
+                listInt.add(number);
+            } catch (NumberFormatException e) {
+                throw new SortException(String.format("Строка в файле не является числом: '%s'", s));
+            }
+        }
+        return listInt;
+    }
+
+    private static <T> void writeOutput(List<T> sortedList, String outputFile) throws SortException {
+        try (PrintWriter writer = new PrintWriter(outputFile)) {
+            for (T e : sortedList) {
+                writer.println(e);
+            }
+        } catch (FileNotFoundException e) {
+            throw new SortException("Ошибка создания выходного файла!");
+        }
     }
 
     private static void printHelp() {
@@ -40,6 +155,7 @@ public class Main {
 
         // Проверка и преобразование строковых значений аргументов программы к нужным типам
         ContentType contentType;
+        //noinspection IfCanBeSwitch
         if (typeArg.equals("-i")) {
             contentType = ContentType.INT;
         } else if (typeArg.equals("-s")) {
@@ -51,6 +167,7 @@ public class Main {
         }
 
         boolean ascendingSort;
+        //noinspection IfCanBeSwitch
         if (sortOrderArg.equals("-a")) {
             ascendingSort = true;
         } else if (sortOrderArg.equals("-d")) {
@@ -61,7 +178,11 @@ public class Main {
             return;
         }
 
-        Main.sortFile(in, out, contentType, ascendingSort);
-
+        try {
+            Main.sortFile(in, out, contentType, ascendingSort);
+            System.out.println("Done!");
+        } catch (SortException e) {
+            System.out.printf("Ошибка: %s", e.getMessage());
+        }
     }
 }
---
+++ Sort/src/ru/academits/erinary/sort/SortException.java
@@ -0,0 +1,7 @@
+package ru.academits.erinary.sort;
+
+public class SortException extends Exception {
+    public SortException(String message) {
+        super(message);
+    }
+}
