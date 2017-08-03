d08eecbcfc1b17906200f1079651152c0dfdd248
Erinary <udalova.lbv@gmail.com>
2017-08-03 18:23:55 +0700

    Изменены некоторые добавленные ранее методы. Добавлены: addAll(c), addAll(index, c), ensureCapacity (minCapacity), clear(), get(), set(index, o), add(index, o)

--- MyArrayList/src/ru/academits/erinary/myarraylist/myarraylist/MyArrayList.java
+++ MyArrayList/src/ru/academits/erinary/myarraylist/myarraylist/MyArrayList.java
@@ -9,7 +9,7 @@ import java.util.ListIterator;
 public class MyArrayList<T> implements List {
     private static final int DEFAULT_CAPACITY = 10;
     private Object[] items;
-    private int listLength = 0;
+    private int size = 0;
 
     public MyArrayList(int initialCapacity) {
         this.items = new Object[initialCapacity];
@@ -23,12 +23,12 @@ public class MyArrayList<T> implements List {
 
     @Override
     public int size() {
-        return listLength;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return listLength == 0;
+        return size == 0;
     }
 
     @Override
@@ -63,11 +63,11 @@ public class MyArrayList<T> implements List {
 
     @Override
     public boolean add(Object o) {
-        if (listLength >= this.items.length) {
+        if (size >= this.items.length) {
             this.increaseCapacity();
         }
-        this.items[listLength] = o;
-        ++listLength;
+        this.items[size] = o;
+        ++size;
         return true;
     }
 
@@ -80,14 +80,14 @@ public class MyArrayList<T> implements List {
     @Override
     public boolean remove(Object o) {
         if (o == null) {
-            for (int i = 0; i < listLength; ++i) {
+            for (int i = 0; i < size; ++i) {
                 if (this.items[i] == null) {
                     this.remove(i);
                     return true;
                 }
             }
         } else {
-            for (int i = 0; i < listLength; ++i) {
+            for (int i = 0; i < size; ++i) {
                 if (this.items[i].equals(o)) {
                     this.remove(i);
                     return true;
@@ -99,44 +99,93 @@ public class MyArrayList<T> implements List {
 
     @Override
     public boolean addAll(Collection c) {
-        return false;
+        if (c.isEmpty()) {
+            throw new NullPointerException("Передаваемая коллекция пуста!");
+        }
+        if (c.size() + size >= this.items.length) {
+            this.ensureCapacity(c.size() - size);
+        }
+        Object[] collectionToCopy = c.toArray();
+        int oldSize = this.size;
+        System.arraycopy(collectionToCopy, 0, this.items, size, collectionToCopy.length);
+        return oldSize != this.size;
     }
 
     @Override
     public boolean addAll(int index, Collection c) {
-        return false;
+        if (c.isEmpty()) {
+            throw new NullPointerException("Передаваемая коллекция пуста!");
+        }
+        if (c.size() + size >= this.items.length) {
+            this.ensureCapacity(c.size() - size);
+        }
+        Object[] collectionToCopy = c.toArray();
+        int oldSize = this.size;
+        if (size - index > 0) {
+            System.arraycopy(this.items, index, this.items, index + collectionToCopy.length, size - index);
+        }
+        System.arraycopy(collectionToCopy, 0, this.items, index, collectionToCopy.length);
+        return oldSize != this.size;
+    }
+
+    public void ensureCapacity(int minCapacity) {
+        if (this.items.length < minCapacity) {
+            Object[] old = this.items;
+            this.items = new Object[this.items.length + minCapacity];
+            System.arraycopy(old, 0, this.items, 0, old.length);
+        }
     }
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < size; ++i) {
+            this.items[i] = null;
+        }
+        size = 0;
     }
 
     @Override
     public Object get(int index) {
-        return null;
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Выход за границы списка");
+        }
+        return this.items[index];
     }
 
     @Override
     public Object set(int index, Object element) {
-        return null;
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Выход за границы списка");
+        }
+        Object oldElement = this.items[index];
+        this.items[index] = element;
+        return oldElement;
     }
 
     @Override
     public void add(int index, Object element) {
-
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Выход за границы списка");
+        }
+        if (size >= this.items.length) {
+            this.increaseCapacity();
+        }
+        if (size - index > 0) {
+            System.arraycopy(this.items, index, this.items, index + 1, size - index);
+        }
+        this.items[index] = element;
     }
 
     @Override
     public Object remove(int index) {
-        if (index >= this.items.length){
-            throw new IndexOutOfBoundsException("Выход за границы массива");
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Выход за границы списка");
         }
         Object removedElement = this.items[index];
-        if (index < listLength - 1) {
-            System.arraycopy(this.items, index + 1, this.items, index, listLength - index - 1);
+        if (index < size - 1) {
+            System.arraycopy(this.items, index + 1, this.items, index, size - index - 1);
         }
-        --listLength;
+        --size;
         return removedElement;
     }
 
