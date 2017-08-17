1dd2e619a918b30046747ddc566905a35edabb3c
Erinary <udalova.lbv@gmail.com>
2017-08-17 13:40:28 +0700

    Исправлено: 1. Лучше размер списка тоже хранить в поле, для оптимизации 2. Кидать просто RuntimeException плохо, надо кидать конкретные исключения. Много где подойдёт исключение для выхода за границы 3. Первый элемент списка правильно называть head, а не first unit. Слово unit не используют, используют node для узла 4. Более просто наоборот реализовать deleteFirst, а из deleteUnit вызвать deleteFirst если удаляют голову списка + В методы, в которые передается индекс, добавлено исключение выхода за границы массива + Добавлены методы удаления узла по значению, вставка узла после указанного узла, удаление узла после указанного узла

--- MyList/src/ru/academits/erinary/list/SimpleLinkedList.java
+++ MyList/src/ru/academits/erinary/list/SimpleLinkedList.java
@@ -2,6 +2,7 @@ package ru.academits.erinary.list;
 
 public class SimpleLinkedList<T> {
     private ListItem head;
+    private int size;
 
     public SimpleLinkedList() {
         this.head = null;
@@ -24,27 +25,25 @@ public class SimpleLinkedList<T> {
 
     /**
      * Получение размера списка
+     *
      * @return размер списка
      */
     public int getSize() {
-        int counter = 0;
-        for (ListItem p = head; p != null; p = p.next) {
-            ++counter;
-        }
-        return counter;
+        return size;
     }
 
     /**
      * Получение узла по индексу
+     *
      * @param index индекс узла
      * @return узел
      */
-    private ListItem getListItem(int index) {
+    private ListItem getNode(int index) {
         if (head == null) {
-            throw new RuntimeException("Список пуст");
+            throw new IndexOutOfBoundsException("Список пуст");
         }
-        if (index < 0) {
-            throw new RuntimeException("Отрицательный индекс");
+        if (index < 0 || index > size - 1) {
+            throw new IndexOutOfBoundsException("Недопустимое значение индекса");
         }
         ListItem p = head;
         for (int i = 0; i < index; ++i) {
@@ -58,38 +57,49 @@ public class SimpleLinkedList<T> {
 
     /**
      * Вставка в начало списка
-     * @param unit значение нового узла
+     *
+     * @param node значение нового узла
      */
-    public void setFirstUnit(T unit) {
-        head = new ListItem(unit, head);
+    public void insertHead(T node) {
+        head = new ListItem(node, head);
+        ++this.size;
     }
 
     /**
-     * Получение первого узла
+     * Получение значения первого узла
+     *
      * @return значение узла
      */
-    public T getFirstUnit() {
-        return getUnit(0);
+    public T getHead() {
+        return getNodeData(0);
     }
 
     /**
      * Получение значения узла по индексу
+     *
      * @param index индекс узла
      * @return значение узла
      */
-    public T getUnit(int index) {
-        ListItem p = this.getListItem(index);
+    public T getNodeData(int index) {
+        if (index < 0 || index > size - 1) {
+            throw new IndexOutOfBoundsException("Недопустимое значение индекса");
+        }
+        ListItem p = this.getNode(index);
         return p.data;
     }
 
     /**
      * Изменение значения узла по индексу
-     * @param index индекс узла
+     *
+     * @param index   индекс узла
      * @param newData новое значение
      * @return старое значение
      */
-    public T setUnit(int index, T newData) {
-        ListItem p = this.getListItem(index);
+    public T setNodeData(int index, T newData) {
+        if (index < 0 || index > size - 1) {
+            throw new IndexOutOfBoundsException("Недопустимое значение индекса");
+        }
+        ListItem p = this.getNode(index);
         T temp = p.data;
         p.data = newData;
         return temp;
@@ -97,52 +107,114 @@ public class SimpleLinkedList<T> {
 
     /**
      * Удаление узла по индексу
+     *
      * @param index индекс узла
      * @return значение удаленного узла
      */
-    public T deleteUnit(int index) {
+    public T deleteNode(int index) {
+        if (index < 0 || index > size - 1) {
+            throw new IndexOutOfBoundsException("Недопустимое значение индекса");
+        }
         T temp;
         if (index == 0) {
-            if (head == null) {
-                throw new RuntimeException("Список пуст");
-            }
-            temp = head.data;
-            head = head.next;
+            temp = this.deleteHead();
         } else {
-            ListItem previous = this.getListItem(index - 1);
+            ListItem previous = this.getNode(index - 1);
             ListItem current = previous.next;
             temp = current.data;
             previous.next = current.next;
+            --this.size;
         }
         return temp;
     }
 
     /**
      * Удаление первого узла
+     *
      * @return выдает значение удаленного узла
      */
-    public T deleteFirst() {
-        return this.deleteUnit(0);
+    public T deleteHead() {
+        T temp;
+        if (head == null) {
+            throw new IndexOutOfBoundsException("Список пуст");
+        }
+        temp = head.data;
+        head = head.next;
+        --this.size;
+        return temp;
     }
 
     /**
      * Вставка элемента по индексу
-     * @param data значение нового узла
+     *
+     * @param data  значение нового узла
      * @param index индекс вставки
      */
-    public void insertUnit(T data, int index) {
+    public void insertNode(T data, int index) {
+        if (index < 0 || index > size - 1) {
+            throw new IndexOutOfBoundsException("Недопустимое значение индекса");
+        }
         if (index == 0) {
-            this.setFirstUnit(data);
+            this.insertHead(data);
         } else {
-            ListItem previous = this.getListItem(index - 1);
+            ListItem previous = this.getNode(index - 1);
             ListItem current = previous.next;
             ListItem newUnit = new ListItem(data);
             previous.next = newUnit;
             newUnit.next = current;
+            ++this.size;
         }
     }
 
+    /**
+     * Удаление узла по значению
+     *
+     * @param data передаваемое значение
+     */
+    public void deleteNodeByData(T data) {
+        if (head == null) {
+            throw new IndexOutOfBoundsException("Список пуст");
+        }
+        for (ListItem p = head, previous = null; true; previous = p, p = p.next) {
+            if (p.data.equals(data) && previous != null) {
+                previous.next = p.next;
+                --this.size;
+                break;
+            } else if (p.data.equals(data) && previous == null) {
+                head = p.next;
+                --this.size;
+                break;
+            } else if (p.next == null) {
+                throw new RuntimeException("Такого значения в списке нет");
+            }
+        }
+    }
 
+    /**
+     * Вставка узла после указанного узла
+     *
+     * @param data          значение нового узла
+     * @param specifiedUnit указанный узел
+     */
+    public void insertNodeAfterSpecifiedOne(T data, ListItem specifiedUnit) {
+        specifiedUnit.next = new ListItem(data, specifiedUnit.next);
+        ++this.size;
+    }
 
-
+    /**
+     * Удаление узла после указанного узла
+     *
+     * @param specifiedUnit указанный узел
+     */
+    public void deleteNodeAfterSpecifiedOne(ListItem specifiedUnit) {
+        if (specifiedUnit.next == null) {
+            throw new IndexOutOfBoundsException("Конец списка, следующий элемент удалить невозможно");
+        }
+        ListItem deletedNode = specifiedUnit.next;
+        specifiedUnit.next = specifiedUnit.next.next;
+        deletedNode.data = null;
+        deletedNode.next = null;
+        --this.size;
+    }
 }
+
